4caec292a406b1fda6e16ad5a8adf626629b0a9c
Korpan-Artem <55976064+Korpan-Artem@users.noreply.github.com>
2019-12-15 11:34:06 +0200

    Logout

--- sing.js
+++ sing.js
@@ -10,6 +10,7 @@
             statusChangeCallback(response);
         });
       };
+      
       (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) {return;}
