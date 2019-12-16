330f611ca5cb6dc3824bbc43f3392f4a75a5bfab
Korpan-Artem <55976064+Korpan-Artem@users.noreply.github.com>
2019-12-16 17:30:30 +0200

    ///

--- sett.js
+++ sett.js
@@ -12,40 +12,12 @@
           version : 'v5.0'  
         });
         FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
+            //statusChangeCallback(response);
         });
       };
 
      
-      function testAPI(){
-        FB.api('/me?fields=name,email', function(response){
-          if(response && !response.error){
-            console.log(response);
-            buildProfile(response);
-          }
-          FB.api('/me/feed', function(response){
-            if(response && !response.error){
-              //buildFeed(response);
-            }
-          });
-        })
-      }
 
-      function setElements(isLoggedIn){
-        if(isLoggedIn){
-          document.getElementById('logout').style.display = 'block';
-          document.getElementById('profile').style.display = 'block';
-          document.getElementById('feed').style.display = 'block';
-          document.getElementById('fb-btn').style.display = 'none';
-          document.getElementById('heading').style.display = 'none';
-        } else {
-          document.getElementById('logout').style.display = 'none';
-          document.getElementById('profile').style.display = 'none';
-          document.getElementById('feed').style.display = 'none';
-          document.getElementById('fb-btn').style.display = 'block';
-          document.getElementById('heading').style.display = 'block';
-        }
-      }
       function logout(){
         FB.logout(function(response){
          // setElements(false);
