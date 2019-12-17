c27cdfdcb7a8f6e5ae96630d06f47b662b108286
Korpan-Artem <55976064+Korpan-Artem@users.noreply.github.com>
2019-12-17 17:29:32 +0200

    ...

--- sett.js
+++ sett.js
@@ -16,6 +16,30 @@
         });
       };
 
+       (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+       function statusChangeCallback(response){
+         if(response.status === 'connected'){
+           console.log('Logged in and authenticated');
+          // setElements(true);
+           //window.location.replace("head-menu.html");
+           testAPI();
+         } else {
+           console.log('Not authenticated');
+           //setElements(false);
+         }
+       }
+      function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
+      }
+
      
 
       function logout(){
