c672e033ecc6e8ee07d7d859e6f7b5cb4d1435b9
Korpan-Artem <55976064+Korpan-Artem@users.noreply.github.com>
2019-12-21 22:42:53 +0200

    ...

--- js/setting.js
+++ js/setting.js
@@ -58,12 +58,13 @@ for (i = 0; i < coll.length; i++) {
          setTimeout(window.location.replace("sing-in.html"),7200);
         });
       }*/
+
              function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
     console.log('statusChangeCallback');
     console.log(response);                   // The current login status of the person.
     if (response.status === 'connected') {   // Logged into your webpage and Facebook.
       testAPI();  
-      window.location.replace("head-menu.html");
+      
     } else {                                 // Not logged into your webpage or we are unable to tell.
       /*document.getElementById('status').innerHTML = 'Please log ' +
         'into this webpage.';*/
